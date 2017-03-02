fe86e7bbf90b909729817b04ef7023c9e8b1c95b
Reyn <jl88744653@gmail.com>
2017-03-03 03:14:03 +0800

    增加 CocoSupport, 方便头文件引用

--- Classes/Pragwork/Base/BaseDefine.h
+++ Classes/Pragwork/Base/BaseDefine.h
@@ -1,15 +1,14 @@
 //
-//  Base.h
+//  BaseDefine.h
 //  Pragwork
 //
 //  Created by Reyn-Mac on 2017/2/27.
 //
 //
 
-#ifndef Base_h
-#define Base_h
+#ifndef BaseDefine_h
+#define BaseDefine_h
 
-#include "BaseLayer.hpp"
 #include "BaseFrame.hpp"
 
-#endif /* Base_h */
+#endif /* BaseDefine_h */
---
+++ Classes/Pragwork/CocoSupport.h
@@ -0,0 +1,31 @@
+//
+//  CocoSupport.h
+//  Pragwork
+//
+//  Created by Reyn-Mac on 2017/3/3.
+//
+//
+
+#ifndef CocoSupport_h
+#define CocoSupport_h
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+#include "extensions/cocos-ext.h"
+#include "audio/include/SimpleAudioEngine.h"
+#include "editor-support/cocostudio/CocoStudio.h"
+#include "base/ccUTF8.h"
+#include "external/ConvertUTF/ConvertUTF.h"
+#include "external/json/rapidjson.h"
+#include "external/tinyxml2/tinyxml2.h"
+#include "storage/local-storage/LocalStorage.h"
+
+using namespace cocos2d;
+using namespace cocos2d::ui;
+using namespace cocos2d::network;
+using namespace cocos2d::experimental;
+using namespace cocos2d::StringUtils;
+using namespace CocosDenshion;
+
+
+#endif /* CocoSupport_h */
