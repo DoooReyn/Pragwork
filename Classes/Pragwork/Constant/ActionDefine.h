24258aa93833b70b222dc22aeac7890844a7d061
Reyn <jl88744653@gmail.com>
2017-03-02 23:44:31 +0800

    Modify actioncode
    Add GetActionByCode method

--- Classes/Pragwork/Constant/ActionDefine.h
+++ Classes/Pragwork/Constant/ActionDefine.h
@@ -9,5 +9,36 @@
 #ifndef ActionDefine_h
 #define ActionDefine_h
 
+USING_NS_CC;
+
+enum class ActionCode : unsigned int {
+    None,
+    EnterBaseFrame,
+    ExitBaseFrame,
+};
+
+static Action* getActionByCode(ActionCode code) {
+    switch (code) {
+        case ActionCode::EnterBaseFrame : {
+            ScaleTo     *a1 = ScaleTo::create(.3f, 1.12f);
+            DelayTime   *a2 = DelayTime::create(.05f);
+            ScaleTo     *a3 = ScaleTo::create(.1f, 1.f);
+            Sequence    *sq = Sequence::create(a1, a2, a3, NULL);
+            sq->setTag(ActionTag::EnterBaseFrame);
+            return sq;
+        }
+        case ActionCode::ExitBaseFrame : {
+            ScaleTo     *a1 = ScaleTo::create(.1f, 1.12f);
+            DelayTime   *a2 = DelayTime::create(.05f);
+            ScaleTo     *a3 = ScaleTo::create(.3f, .5f);
+            RemoveSelf  *a4 = RemoveSelf::create();
+            Sequence    *sq = Sequence::create(a1, a2, a3, a4, NULL);
+            sq->setTag(ActionTag::ExitBaseFrame);
+            return sq;
+        }
+        default:
+            return NULL;
+    }
+}
 
 #endif /* ActionDefine_h */
