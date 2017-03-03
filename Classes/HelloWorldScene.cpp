e747f025ffee3dd9d58dbe3db449f1e43c41a4f5
Reyn <jl88744653@gmail.com>
2017-03-03 23:18:21 +0800

    remove unused variables

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -30,9 +30,6 @@ bool HelloWorld::init()
     {
         return false;
     }
-    
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
